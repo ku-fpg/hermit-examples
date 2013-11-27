66239fdbf73972a1e6097e0d4f7ecf441ff57647
Neil Sculthorpe <neil@ittc.ku.edu>
2013-11-27 15:41:56 -0600

    Added notes on reasoning.

--- Bird-book/century/Century.hs
+++ Bird-book/century/Century.hs
@@ -9,36 +9,39 @@ data Candidate
 
 data Value
 
-{-# RULES "comp-assoc-L"  forall f g h. (f . g) . h = f . (g . h)    #-}
-{-# RULES "comp-assoc-R"  forall f g h. f . (g . h) = (f . g) . h    #-}
-{-# RULES "map-fusion"    forall f g.   map (f . g) = map f . map g  #-}
-{-# RULES "map-id"                      map id      = id             #-}
-{-# RULES "id-map"                      id          = map id         #-}
-{-# RULES "comp-id-L"     forall f.     id . f      = f              #-}
-{-# RULES "comp-R-id"     forall x.     extend x    = extend x . id  #-}
+{- RULES "comp-assoc-L"  forall f g h. (f . g) . h = f . (g . h)    #-}
+{- RULES "comp-assoc-R"  forall f g h. f . (g . h) = (f . g) . h    #-}
+{- RULES "map-fusion"    forall f g.   map (f . g) = map f . map g  #-}
+{- RULES "map-id"                      map id      = id             #-}
+{- RULES "id-map"                      id          = map id         #-}
+{- RULES "comp-id-L"     forall f.     id . f      = f              #-}
+{- RULES "comp-R-id"     forall x.     extend x    = extend x . id  #-}
 
-{-# RULES "comp-hack"     forall f g h k l.  f . g . h . k . l  = (f . g . h . k) . l  #-}
+{- RULES "comp-hack"     forall f g h k l.  f . g . h . k . l  = (f . g . h . k) . l  #-}
 
-{-# RULES "foldr-fusion1"  filter (ok . value)    . foldr extend  [] = foldr extend' []  #-}
-{-# RULES "foldr-fusion2"  map (fork (id, value)) . foldr extend' [] = foldr expand []   #-}
+{- RULES "foldr-fusion1"  filter (ok . value)    . foldr extend  [] = foldr extend' []  #-}
+{- RULES "foldr-fusion2"  map (fork (id, value)) . foldr extend' [] = foldr expand []   #-}
 
-{-# RULES "6.2"            filter (good . value)          = filter (good . value) . filter (ok . value)           #-}
-{-# RULES "6.3"  forall x. filter (ok . value) . extend x = filter (ok . value) . extend x . filter (ok . value)  #-}
-{-# RULES "6.4"  forall x. map value . extend x           = modify x . map value                                  #-}
+{- RULES "6.2"            filter (good . value)          = filter (good . value) . filter (ok . value)           #-}
+{- RULES "6.3"  forall x. filter (ok . value) . extend x = filter (ok . value) . extend x . filter (ok . value)  #-}
+{- RULES "6.4"  forall x. map value . extend x           = modify x . map value                                  #-}
 
-{-# RULES "6.5a"  forall f g.     fst . fork (f,g)     = f                                                 #-}
-{-# RULES "6.5b"  forall f g.     snd . fork (f,g)     = g                                                 #-}
-{-# RULES "6.6"   forall f g h.   fork (f,g) . h       = fork (f . h, g . h)                               #-}
-{-# RULES "6.7"   forall f g h k. fork (f . h, g . k)  = cross (f,g) . fork (h,k)                          #-}
-{-# RULES "6.8"   forall f g.     fork (map f , map g) = unzip . map (fork (f , g))                        #-}
-{-# RULES "6.9"   forall f g.     map (fork (f , g))   = zip . fork (map f , map g)                        #-}
-{-# RULES "6.10"  forall f g p.   map (fork (f,g)) . filter (p . g) = filter (p . snd) . map (fork (f,g))  #-}
+{- RULES "6.5a"  forall f g.     fst . fork (f,g)     = f                                                 #-}
+{- RULES "6.5b"  forall f g.     snd . fork (f,g)     = g                                                 #-}
+{- RULES "6.6"   forall f g h.   fork (f,g) . h       = fork (f . h, g . h)                               #-}
+{- RULES "6.7"   forall f g h k. fork (f . h, g . k)  = cross (f,g) . fork (h,k)                          #-}
+{- RULES "6.8"   forall f g.     fork (map f , map g) = unzip . map (fork (f , g))                        #-}
+{- RULES "6.9"   forall f g.     map (fork (f , g))   = zip . fork (map f , map g)                        #-}
+{- RULES "6.10"  forall f g p.   map (fork (f,g)) . filter (p . g) = filter (p . snd) . map (fork (f,g))  #-}
+
+{- RULES "expand-spec"  forall x. map (fork (id, value)) . extend' x = expand x . map (fork (id, value)) #-}
 
 -------------------------------------------------
 
 solutions :: Data -> [Candidate]
 solutions = filter (good . value) . candidates
 
+-- Assumption 6.1
 candidates :: Data -> [Candidate]
 candidates = foldr extend []
 
@@ -69,13 +72,6 @@ extend' x = filter (ok . value) . extend x
 
 expand :: Datum -> [(Candidate, Value)] -> [(Candidate, Value)]
 expand x = filter (ok . snd) . zip . cross (extend x, modify x) . unzip
-
--- This proof derives a definition of expand that validates RULE "foldr-fusion2"
-{-# RULES "expand-spec"  forall x. map (fork (id, value)) . extend' x = expand x . map (fork (id, value)) #-}
-expandSpecProof :: Datum -> [Candidate] -> [(Candidate, Value)]
-expandSpecProof x =  map (fork (id, value)) . extend' x
-                --   <=>
-                --   expand x . map (fork (id, value))
 
 modify :: Datum -> [Value] -> [Value]
 modify = undefined
---
+++ Bird-book/century/Proofs.hs
@@ -0,0 +1,36 @@
+module Proofs where
+
+import Century
+
+-------------------------------------------------
+
+-- These definitions are just for testing the proofs.
+
+-- This proof derives a definition of expand that validates RULE "foldr-fusion2"
+-- See file "FusionPrecondition"
+proofExpandSpec :: Datum -> [Candidate] -> [(Candidate, Value)]
+proofExpandSpec x =  map (fork (id, value)) . extend' x
+                --   <=>
+                --   expand x . map (fork (id, value))
+
+proof_6_5a :: (a -> b) -> (a -> c) -> a -> b
+proof_6_5a = \ f g -> fst . fork (f,g)
+           -- =>
+           -- f
+
+proof_6_5b :: (a -> b) -> (a -> c) -> a -> c
+proof_6_5b = \ f g -> snd . fork (f,g)
+           -- =>
+           -- g
+
+proof_6_6 :: (b -> c) -> (b -> d) -> (a -> b) -> a -> (c,d)
+proof_6_6 = \ f g h -> fork (f,g) . h
+          -- =>
+          -- fork (f . h, g . h)
+
+proof_6_7 :: (b -> y) -> (c -> z) -> (a -> b) -> (a -> c) -> a -> (y,z)
+proof_6_7 = \ f g h k -> fork (f . h, g . k)
+          -- =>
+          -- cross (f,g) . fork (h,k)
+
+-------------------------------------------------
